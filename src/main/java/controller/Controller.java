a86f41f9e8e8ffe1bb47ec8508fe1753f203ba57
asdsajt <egyed.vince1998@gmail.com>
2021-03-27 17:00:29 +0100

    controller spreadsheet generating, listeners adding

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,73 +1,40 @@
 package controller;
 
-import javafx.beans.InvalidationListener;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.scene.control.TablePosition;
-import javafx.scene.control.TextField;
-import org.controlsfx.control.spreadsheet.Grid;
 import org.controlsfx.control.spreadsheet.GridBase;
 import org.controlsfx.control.spreadsheet.SpreadsheetCell;
 import org.controlsfx.control.spreadsheet.SpreadsheetCellType;
 import view.View;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 public class Controller {
 
     private View view;
-    private InvalidationListener cellDisableListener, allocationSelectorListener;
+    private SpreadSheetClickListeners spreadSheetListeners;
 
-    private String blockedStyle, normalStyle, selectedCellStyle;
 
     public Controller(View view) {
         this.view = view;
-
-        blockedStyle = "-fx-background-color: #d95a5a;";
-        normalStyle = "-fx-background-color: transparent;";
-        selectedCellStyle = "-fx-backgroud-color: 9cfc94;";
-
+        spreadSheetListeners = new SpreadSheetClickListeners(this.view.getRoomSpreadSheetView());
         view.init();
-        configureSpreadSheet(Integer.parseInt(view.getRowNumberTextField().getText()), Integer.parseInt(view.getColumnNumberTextField().getText()));
+
+        configureSpreadSheet(Integer.parseInt(view.getRowNumberTextField().getText()),
+                Integer.parseInt(view.getColumnNumberTextField().getText()));
         createListeners();
     }
 
     private void createListeners() {
-        cellDisableListener = observable -> {
-            try {
-                TablePosition cellpos = view.getRoomSpreadSheetView().getSelectionModel().getSelectedCells().get(0);
-                SpreadsheetCell cell = view.getRoomSpreadSheetView().getGrid().getRows().get(cellpos.getRow()).get(cellpos.getColumn());
-                if (cell.getStyle().equals(normalStyle)) {
-                    cell.setStyle(blockedStyle);
-                } else {
-                    cell.setStyle(normalStyle);
-                }
-
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        };
-
-        allocationSelectorListener = observable -> {
-            try {
-                TablePosition cellpos = view.getRoomSpreadSheetView().getSelectionModel().getSelectedCells().get(0);
-                SpreadsheetCell cell = view.getRoomSpreadSheetView().getGrid().getRows().get(cellpos.getRow()).get(cellpos.getColumn());
-                if (cell.getStyle().equals(normalStyle)) {
-                    cell.setStyle(selectedCellStyle);
-                } else if (cell.getStyle().equals(selectedCellStyle)){
-                    cell.setStyle(normalStyle);
-                }
-
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        };
+        view.getRoomSpreadSheetView().getSelectionModel().getSelectedCells().addListener(spreadSheetListeners.getAllocationSelectorListener());
 
         view.getRowNumberTextField().textProperty().addListener((observable, oldValue, newValue) -> {
             try {
                 if (!newValue.matches("\\d*")) {
                     view.getRowNumberTextField().setText(newValue.replaceAll("[^\\d]", ""));
                 }
-                if(!newValue.equals(""))
-                    configureSpreadSheet(Integer.parseInt(view.getRowNumberTextField().getText()), Integer.parseInt(view.getColumnNumberTextField().getText()));
+                if(!newValue.replaceAll("[^\\d]", "").equals(""))
+                    configureSpreadSheet(Integer.parseInt(newValue.replaceAll("[^\\d]", "")), Integer.parseInt(view.getColumnNumberTextField().getText()));
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
@@ -78,47 +45,85 @@ public class Controller {
                 if (!newValue.matches("\\d*")) {
                     view.getColumnNumberTextField().setText(newValue.replaceAll("[^\\d]", ""));
                 }
-                if(!newValue.equals(""))
-                    configureSpreadSheet(Integer.parseInt(view.getRowNumberTextField().getText()), Integer.parseInt(view.getColumnNumberTextField().getText()));
+                if(!newValue.replaceAll("[^\\d]", "").equals(""))
+                    configureSpreadSheet(Integer.parseInt(view.getRowNumberTextField().getText()), Integer.parseInt(newValue.replaceAll("[^\\d]", "")));
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        view.getDisableSeatsCheckBox().selectedProperty().addListener(observable -> {
+            try {
+                if(view.getDisableSeatsCheckBox().isSelected()) {
+                    view.getRoomSpreadSheetView().getSelectionModel().getSelectedCells().removeListener(spreadSheetListeners.getAllocationSelectorListener());
+                    view.getRoomSpreadSheetView().getSelectionModel().getSelectedCells().addListener(spreadSheetListeners.getCellDisableListener());
+                } else {
+                    view.getRoomSpreadSheetView().getSelectionModel().getSelectedCells().removeListener(spreadSheetListeners.getCellDisableListener());
+                    view.getRoomSpreadSheetView().getSelectionModel().getSelectedCells().addListener(spreadSheetListeners.getAllocationSelectorListener());
+                }
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
         });
     }
 
+
+    /**
+     * SpreaSheetView konfigurálása
+     *  -
+     * @param rowNumber
+     * @param columnNumber
+     */
     private void configureSpreadSheet(int rowNumber, int columnNumber) {
         view.getRoomSpreadSheetView().setGrid(null);
-        view.getRoomSpreadSheetView().setGrid(createGridBase(rowNumber, columnNumber));
+
+        GridBase gridBase = createGrid(rowNumber, columnNumber);
+
+        /**
+         * Automatikus méretezés, hogy ne kelljen görgetni
+         */
+        AtomicReference<Double> maxSize = new AtomicReference<>();
+        maxSize.set(66.0);
+        while ((rowNumber >= columnNumber ? rowNumber : columnNumber) * maxSize.get() > 660 && maxSize.get() != 35) {
+            maxSize.set(maxSize.get()-1);
+        }
+
+        gridBase.setRowHeightCallback(param -> {
+            try {
+                return maxSize.get();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        view.getRoomSpreadSheetView().setGrid(gridBase);
         view.getRoomSpreadSheetView().getColumns().forEach(col -> {
             try {
-                col.setPrefWidth(50);
+                col.setMaxWidth(maxSize.get());
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
         });
     }
 
-    private Grid createGridBase(int rowNumber, int columnNumber) {
+    /**
+     * SpreadSheetView-ban megjelenő Griden lévő cellák inicializálása
+     * @param rowNumber: sor szám
+     * @param columnNumber: oszlop szám
+     * @return
+     */
+    private GridBase createGrid(int rowNumber, int columnNumber) {
         GridBase gridBase = new GridBase(rowNumber, columnNumber);
-        gridBase.setRowHeightCallback(param -> {
-            try {
-                return 50.0;
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        });
-
         ObservableList<ObservableList<SpreadsheetCell>> rows = FXCollections.observableArrayList();
         for(int i = 0; i < rowNumber; i++) {
             ObservableList<SpreadsheetCell> rowList = FXCollections.observableArrayList();
             for(int j = 0; j < columnNumber; j++) {
                 SpreadsheetCell cell = SpreadsheetCellType.STRING.createCell(i, j, 1, 1, "");
-                cell.setStyle(normalStyle);
+                cell.setStyle(spreadSheetListeners.getNormalStyle());
                 rowList.add(cell);
             }
             rows.add(rowList);
         }
-
         gridBase.setRows(rows);
         return gridBase;
     }
