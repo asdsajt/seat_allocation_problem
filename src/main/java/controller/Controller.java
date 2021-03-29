1f8b09ff0e5d84236e0515208b1dcb8e3dbe13fb
asdsajt <egyed.vince1998@gmail.com>
2021-03-29 17:52:41 +0200

    refresh spreadsheet bug fix

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,6 +1,5 @@
 package controller;
 
-import com.mongodb.lang.Nullable;
 import database.DatabaseHandler;
 import globalControls.AlertMaker;
 import globalControls.CellStyles;
@@ -11,7 +10,6 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.Alert;
 import javafx.stage.Modality;
 import model.Room;
-import model.Seat;
 import model.Theater;
 import model.utils.enums.SeatStatus;
 import org.controlsfx.control.spreadsheet.GridBase;
@@ -27,7 +25,6 @@ import solver.greedy.GreedySolver;
 import view.View;
 
 import java.util.ArrayList;
-import java.util.Locale;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class Controller {
@@ -42,9 +39,10 @@ public class Controller {
         spreadSheetListeners = new SpreadSheetClickListeners(this.view.getRoomSpreadSheetView(), this.view.getRoomSpreadSheetView().getScene().getWindow());
 
         initComboBoxes();
-        configureSpreadSheet(Integer.parseInt(view.getRowNumberLabel().getText()),
-                Integer.parseInt(view.getColumnNumberLabel().getText()), null);
         createListeners();
+        configureSpreadSheet(Integer.parseInt(view.getRowNumberLabel().getText()),
+                Integer.parseInt(view.getColumnNumberLabel().getText()));
+
     }
 
     /**
@@ -92,11 +90,11 @@ public class Controller {
     }
 
     private void saveCurrentRoom(ActionEvent actionEvent) {
-        if(spreadSheetListeners.getCurrentRoom() == null) {
+        if (spreadSheetListeners.getCurrentRoom() == null) {
             SeatStatus[][] seatStatuses = new SeatStatus[view.getRoomSpreadSheetView().getGrid().getRowCount()][view.getRoomSpreadSheetView().getGrid().getColumnCount()];
             for (ObservableList<SpreadsheetCell> row : view.getRoomSpreadSheetView().getGrid().getRows()) {
                 for (SpreadsheetCell cell : row) {
-                    if(cell.getStyle().equals(CellStyles.BLOCKED_CELL_STYLE))
+                    if (cell.getStyle().equals(CellStyles.BLOCKED_CELL_STYLE))
                         seatStatuses[cell.getRow()][cell.getColumn()] = SeatStatus.Removed;
                     else
                         seatStatuses[cell.getRow()][cell.getColumn()] = SeatStatus.Empty;
@@ -142,31 +140,31 @@ public class Controller {
         String theaterName = view.getTheaterComboBox().getValue();
 
         // Amikor egy szoba már meg van nyiva és egy másik színházat választok, akkor a room Combo Box null
-        if(!(roomName == null) && !roomName.equals("Válasszon termet...") && !theaterName.equals("Válasszon színházat...")) {
+        if (!(roomName == null) && !roomName.equals("Válasszon termet...") && !theaterName.equals("Válasszon színházat...")) {
 
             String theaterId = getTheaterIdByName(theaterName);
 
             DatabaseHandler dbHandler = new DatabaseHandler();
             Room[] rooms = dbHandler.getRoomsByTheaterId(theaterId); //null ha nincs ilyen szinházId-val terem
             for (Room room : rooms) {
-                if (room.getName().equals(roomName)){
+                if (room.getName().equals(roomName)) {
                     System.out.println(room.getRows()[0][0]);
                     spreadSheetListeners.setCurrentRoom(room);
                 }
             }
             if (spreadSheetListeners.getCurrentRoom() != null) {
-                view.getRowNumberLabel().setText(spreadSheetListeners.getCurrentRoom().getRowNum()+"");
-                view.getColumnNumberLabel().setText(spreadSheetListeners.getCurrentRoom().getColumnNum()+"");
-                configureSpreadSheet(spreadSheetListeners.getCurrentRoom().getRowNum(), spreadSheetListeners.getCurrentRoom().getColumnNum(), spreadSheetListeners.getCurrentRoom().getRows());
+                configureSpreadSheet(spreadSheetListeners.getCurrentRoom().getRowNum(), spreadSheetListeners.getCurrentRoom().getColumnNum());
+                view.getRowNumberLabel().setText(spreadSheetListeners.getCurrentRoom().getRowNum() + "");
+                view.getColumnNumberLabel().setText(spreadSheetListeners.getCurrentRoom().getColumnNum() + "");
             }
         }
     }
 
-    private String getTheaterIdByName(String theaterName){
+    private String getTheaterIdByName(String theaterName) {
         DatabaseHandler dbHandler = new DatabaseHandler();
         Theater[] theaters = dbHandler.getAllTheater();
         for (Theater theater : theaters) {
-            if (theater.getName().equals(theaterName)){
+            if (theater.getName().equals(theaterName)) {
                 return theater.getId();
             }
         }
@@ -179,7 +177,7 @@ public class Controller {
         String theaterName = view.getTheaterComboBox().getValue();
         String theaterId = "";
 
-        if(!theaterName.equals("Válasszon színházat...")) {
+        if (!theaterName.equals("Válasszon színházat...")) {
             theaterId = getTheaterIdByName(theaterName);
 
             DatabaseHandler dbHandler = new DatabaseHandler();
@@ -190,6 +188,7 @@ public class Controller {
         }
 
         view.getRoomComboBox().setItems(FXCollections.observableList(roomNames));
+        view.getRoomComboBox().setValue("Válasszon termet...");
     }
 
     private void addTheater(ActionEvent actionEvent) {
@@ -224,6 +223,7 @@ public class Controller {
     }
 
     private void solverPressed(ActionEvent actionEvent) {
+        view.getRoomSpreadSheetView().getSelectionModel().clearSelection();
         if (view.getDisableSeatsCheckBox().isSelected()) {
             view.getRoomSpreadSheetView().getSelectionModel().getSelectedCells().removeListener(spreadSheetListeners.getCellDisableListener());
         } else {
@@ -276,15 +276,21 @@ public class Controller {
      * - gridben a cellák magasságát állítja
      * - spreadSheethez hozzáadja a gridet
      * - spreadSheet oszlopainak méretezése
-     *  @param rowNumber :    gridben lévő sorok száma
+     *
+     * @param rowNumber    :    gridben lévő sorok száma
      * @param columnNumber : gridben lévő oszlopok száma
-     * @param rows: aktuális terem
      */
-    private void configureSpreadSheet(int rowNumber, int columnNumber, @Nullable Seat[][] rows) {
+    private void configureSpreadSheet(int rowNumber, int columnNumber) {
+        view.getRoomSpreadSheetView().getSelectionModel().clearSelection();
+        if (view.getDisableSeatsCheckBox().isSelected()) {
+            view.getRoomSpreadSheetView().getSelectionModel().getSelectedCells().removeListener(spreadSheetListeners.getCellDisableListener());
+        } else {
+            view.getRoomSpreadSheetView().getSelectionModel().getSelectedCells().removeListener(spreadSheetListeners.getAllocationSelectorListener());
+        }
+        view.getRoomSpreadSheetView().getGrid().getRows().clear();
         view.getRoomSpreadSheetView().setGrid(null);
 
-        GridBase gridBase = createGrid(rowNumber, columnNumber, rows);
-
+        GridBase gridBase = createGrid(rowNumber, columnNumber);
         /**
          * Automatikus méretezés, hogy ne kelljen görgetni
          */
@@ -311,24 +317,29 @@ public class Controller {
                 throw new RuntimeException(e);
             }
         });
+
+        if (view.getDisableSeatsCheckBox().isSelected()) {
+            view.getRoomSpreadSheetView().getSelectionModel().getSelectedCells().addListener(spreadSheetListeners.getCellDisableListener());
+        } else {
+            view.getRoomSpreadSheetView().getSelectionModel().getSelectedCells().addListener(spreadSheetListeners.getAllocationSelectorListener());
+        }
     }
 
     /**
      * SpreadSheetView-ban megjelenő Griden lévő cellák inicializálása
      *
-     * @param rowNumber :    sor szám
+     * @param rowNumber    :    sor szám
      * @param columnNumber : oszlop szám
-     * @param currentRows: ha van betöltött terem, akkor a cella tiltást is elvégzi
      * @return
      */
-    private GridBase createGrid(int rowNumber, int columnNumber, @Nullable Seat[][] currentRows) {
+    private GridBase createGrid(int rowNumber, int columnNumber) {
         GridBase gridBase = new GridBase(rowNumber, columnNumber);
         ObservableList<ObservableList<SpreadsheetCell>> rows = FXCollections.observableArrayList();
         for (int i = 0; i < rowNumber; i++) {
             ObservableList<SpreadsheetCell> rowList = FXCollections.observableArrayList();
             for (int j = 0; j < columnNumber; j++) {
                 SpreadsheetCell cell = SpreadsheetCellType.STRING.createCell(i, j, 1, 1, "");
-                if(currentRows != null && currentRows[i][j].getStatus() == SeatStatus.Removed)
+                if (spreadSheetListeners.getCurrentRoom() != null && spreadSheetListeners.getCurrentRoom().getSeat(i, j).getStatus() == SeatStatus.Removed)
                     cell.setStyle(CellStyles.BLOCKED_CELL_STYLE);
                 else
                     cell.setStyle(CellStyles.NORMAL_CELL_STYLE);
